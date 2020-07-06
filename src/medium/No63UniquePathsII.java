5bd39d5c4b3e5a76dcbea91cc63b1993ee701865
XanderYe <XanderYe@outlook.com>
2020-07-06 18:05:36 +0800

    提交leetcode代码

---
+++ src/medium/No63UniquePathsII.java
@@ -0,0 +1,47 @@
+package medium;
+
+/**
+ * Created on 2020/7/6.
+ *
+ * @author 叶振东
+ */
+public class No63UniquePathsII {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                break;
+            } else {
+                dp[0][j] = 1;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] mn = new int[][]{
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}
+        };
+        System.out.println(new No63UniquePathsII().uniquePathsWithObstacles(mn));
+    }
+}
