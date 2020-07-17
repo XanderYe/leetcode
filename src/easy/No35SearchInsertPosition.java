624c28eed8e3f6f97e33d50f474353c5ce1cdaeb
XanderYe <XanderYe@outlook.com>
2020-07-17 09:20:11 +0800

    提交leetcode代码

--- src/easy/No35SearchInsertPosition.java
+++ src/easy/No35SearchInsertPosition.java
@@ -5,10 +5,26 @@ package easy;
  */
 public class No35SearchInsertPosition {
     public static void main(String[] args) {
-        int[] nums={3,5,7,9};
-        System.out.println(new No35SearchInsertPosition().searchInsert(nums,1));
+        int[] nums={1, 3, 5, 6};
+        System.out.println(new No35SearchInsertPosition().searchInsert(nums,0));
     }
+
+
     public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    public int searchInsert2(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             if(target<=nums[i]){
                 return i;
