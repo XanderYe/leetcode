2cabb0d3522ea6124c79c704beff59580a568224
XanderYe <XanderYe@outlook.com>
2020-08-05 09:04:55 +0800

    提交leetcode代码

---
+++ src/medium/HouseRobberIII.java
@@ -0,0 +1,31 @@
+package medium;
+
+import structure.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created on 2020/8/5.
+ *
+ * @author 叶振东
+ */
+public class HouseRobberIII {
+    Map<TreeNode, Integer> f = new HashMap<>();
+    Map<TreeNode, Integer> g = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+    }
+}
