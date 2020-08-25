abb0eec2278a84d4fa176cb5f6ae33e3a5a90f21
XanderYe <XanderYe@outlook.com>
2020-08-25 09:07:26 +0800

    提交leetcode代码

---
+++ src/medium/No491IncreasingSubsequences.java
@@ -0,0 +1,36 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 2020/8/25.
+ *
+ * @author 叶振东
+ */
+public class No491IncreasingSubsequences {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        dfs(0, Integer.MIN_VALUE, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int last, int[] nums) {
+        if (cur == nums.length) {
+            if (temp.size() >= 2) {
+                ans.add(new ArrayList<Integer>(temp));
+            }
+            return;
+        }
+        if (nums[cur] >= last) {
+            temp.add(nums[cur]);
+            dfs(cur + 1, nums[cur], nums);
+            temp.remove(temp.size() - 1);
+        }
+        if (nums[cur] != last) {
+            dfs(cur + 1, last, nums);
+        }
+    }
+}
