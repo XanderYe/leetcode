fc9372027830ee113d3b8e41c85f4a7b8c910496
XanderYe <XanderYe@outlook.com>
2020-09-01 09:16:47 +0800

    提交leetcode代码

---
+++ src/medium/No486PredictTheWinner.java
@@ -0,0 +1,22 @@
+package medium;
+
+/**
+ * Created on 2020/9/1.
+ *
+ * @author 叶振东
+ */
+public class No486PredictTheWinner {
+    public boolean PredictTheWinner(int[] nums) {
+        int length = nums.length;
+        int[] dp = new int[length];
+        for (int i = 0; i < length; i++) {
+            dp[i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[length - 1] >= 0;
+    }
+}
