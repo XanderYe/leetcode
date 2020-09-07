b48e4cdb155fa2046f4fe12685b6fe2a1399b245
XanderYe <XanderYe@outlook.com>
2020-09-07 09:15:19 +0800

    347. 前 K 个高频元素

---
+++ src/medium/No347TopKFrequentElements.java
@@ -0,0 +1,39 @@
+package medium;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * Created on 2020/9/7.
+ * @see <a href="https://leetcode-cn.com/problems/top-k-frequent-elements/comments/">347. 前 K 个高频元素</a>
+ * @author 叶振东
+ */
+public class No347TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(m -> m[1]));
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+}
